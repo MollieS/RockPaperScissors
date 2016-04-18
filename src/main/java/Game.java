e78f11076d305f5143830361a86e2601975fb5d0
Mollie Stephenson <molliestephenson@Mollies-MBP.default>
2016-04-18 18:13:16 +0100

    game validates input and prompts for correct input if not valid

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -7,4 +7,17 @@ public class Game {
   public String promptForInput() {
     return "Please choose:\n1: Rock\n2: Paper\n3: Scissors";
   }
+
+  public boolean validInput(String input) {
+    if (input != "1" && input != "2" && input != "3") return false;
+    return true;
+  }
+
+  public String input(String word) {
+    if (validInput(word)) {
+      return null;
+    } else {
+      return "Please choose 1, 2 or 3";
+    }
+  }
 }
--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -2,20 +2,43 @@ import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import org.junit.Before;
 
 public class GameTest {
 
+  private Game game;
+
+  @Before
+  public void setUp() {
+    game = new Game();
+  }
+
   @Test
   public void greetsUser() {
-    Game game = new Game();
     String greeting = game.greet();
     assertThat(greeting, containsString("Welcome to RockPaperScissors!"));
   }
 
   @Test
   public void asksForInput() {
-    Game game = new Game();
     String prompt = game.promptForInput();
     assertThat(prompt, containsString("Please choose:\n1: Rock\n2: Paper\n3: Scissors"));
+  }
+
+  @Test
+  public void onlyTakesNumbers() {
+    assertFalse(game.validInput("Rock"));
+    assertTrue(game.validInput("1"));
+    assertTrue(game.validInput("2"));
+    assertTrue(game.validInput("3"));
+    assertFalse(game.validInput("4"));
+  }
+
+  @Test
+  public void promptsForValidInput() {
+    String error = game.input("Rock");
+    assertThat(error, containsString("Please choose 1, 2 or 3"));
   }
 }
